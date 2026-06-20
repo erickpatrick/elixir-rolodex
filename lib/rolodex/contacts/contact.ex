9d02e21f92d864638509a4315fe3f7409c6023fa
erick patrick <github@erickpatrick.net>
2026-06-20 18:28:31 +0200

    allows editing contacts

--- lib/rolodex/contacts/contact.ex
+++ lib/rolodex/contacts/contact.ex
@@ -19,7 +19,7 @@ defmodule Rolodex.Contacts.Contact do
     |> cast(attrs, [:name, :email, :favorite, :notes, :title, :birthday])
     |> validate_required([:name])
     |> validate_length(:name, max: 100)
-    |> validate_length(:email, max: 100)
+    |> validate_length(:email, max: 100, min: 3)
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
     |> unsafe_validate_unique(:email, Rolodex.Repo)
     |> unique_constraint(:email)
